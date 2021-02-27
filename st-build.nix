214ca0e904ace0981bdcb4ab62bcd3d534277c5a
Axel Gschaider <axel.gschaider@posteo.de>
2021-02-27 08:24:05 +0100

    setup nix for local development

--- st-build.nix
+++ st-build.nix
@@ -3,12 +3,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
     name = "st";
     version = "0.99";
-    src = fetchFromGitHub {
-	owner = "pacordova";
-	repo = "st";
-	rev = "9e4d29c2c4ecb2cc8ac02673145e49ac54c97f5e";
-        sha256 = "0zdhgzwvw948pp2v3vx8dkr7jypyhlr8hyfr1apgi2gajbkjhp18";
-    };
+    src = /home/agl/github/st;
     nativeBuildInputs = [ pkgconfig ];
     buildInputs = [ xorg.libX11 ncurses xorg.libXext xorg.libXft fontconfig ];
     installPhase = ''
